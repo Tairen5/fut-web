0c39e42e22a789dfb7d94ca6b3666a96dc146ce9
JaviMC50 <pepeviyuelarito@gmail.com>
2026-07-23 20:28:02 +0100

    Increase playersicon default size

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -10,7 +10,7 @@ const NAV_ITEMS = [
       <img
         src="/assets/playersicon.png"
         alt="Players"
-        style={{ width: '22px', height: '22px', objectFit: 'contain', opacity: 0.8 }}
+        style={{ width: '36px', height: '36px', objectFit: 'contain', opacity: 0.8 }}
       />
     ),
   },
