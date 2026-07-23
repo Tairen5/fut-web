99f2a1e9a14694b76da242723b7fc45a5ee8031c
JaviMC50 <pepeviyuelarito@gmail.com>
2026-07-23 20:03:35 +0100

    Standardize icon sizes and add active scale animation

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -10,7 +10,7 @@ const NAV_ITEMS = [
       <img
         src="/assets/playersicon.png"
         alt="Players"
-        style={{ width: '64px', height: '64px', objectFit: 'contain', opacity: 0.8 }}
+        style={{ width: '22px', height: '22px', objectFit: 'contain', opacity: 0.8 }}
       />
     ),
   },
