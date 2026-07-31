f1b26382ebdcc53b63d2e0b9895d7e53e563a18c
JaviMC50 <pepeviyuelarito@gmail.com>
2026-07-31 19:46:46 +0100

    arreglo admin

--- frontend/src/components/admin/AdminRoute.jsx
+++ frontend/src/components/admin/AdminRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import useAuthStore from '../store/useAuthStore';
+import useAuthStore from '../../store/useAuthStore';
 
 export default function AdminRoute({ children }) {
   const user = useAuthStore((s) => s.user);
